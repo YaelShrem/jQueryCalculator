96408e8c9420c903aea8938d32c9e5013d86ed91
YaelBusso <68850686+YaelBusso@users.noreply.github.com>
2020-11-03 13:18:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+$(document).ready(function(e) {
+    //Dark Mode
+    $('#mode').click(function(e) {
+        e=e||window.event;
+        document.documentElement.classList.toggle('dark-mode');
+    })
+    //Duplicate
+    $('#duplicate').click(function(e) {
+        console.log("clicked");
+        let calculator=$('.calculator').clone();
+        $('.duplicate').html(calculator);
+    })    
+    //Calculator
+    $('#1,#2,#3,#4,#5,#6,#7,#8,#9,#0').click(function(){
+		var v = $(this).val();
+		$('#answer').val($('#answer').val() + v);	
+	});
+	$('#C').click(function(){
+		$('#answer').val('');
+		$('#operation').val('');
+		$('#operation').removeClass('activeAnswer');
+		$('#equals').attr('onclick','');
+	});
+	$('#plus').click(function(e) { 
+	
+		if($('#answer').val() == ''){
+			return false;
+			$('#equals').attr('onclick','');
+		}
+		else if ( $('#operation').attr('class') == 'activeAnswer') {
+			$('#operation').val( $('#operation').val() + $('#plus').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+		else{
+			$('#operation').val( $('#operation').val() + $('#answer').val() + $('#plus').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+    });
+	$('#subtract').click(function(e) { 
+	
+		if($('#answer').val() == ''){
+			return false;	
+			$('#equals').attr('onclick','');
+		}
+		else if ( $('#operation').attr('class') == 'activeAnswer') {
+			$('#operation').val( $('#operation').val() + $('#subtract').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+		else{
+			$('#operation').val( $('#operation').val() + $('#answer').val() + $('#subtract').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+    });
+	$('#divide').click(function(e) { 
+	
+		if($('#answer').val() == ''){
+			return false;	
+			$('#equals').attr('onclick','');
+		}
+		else if ( $('#operation').attr('class') == 'activeAnswer') {
+			$('#operation').val( $('#operation').val() + $('#divide').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+		else{
+			$('#operation').val( $('#operation').val() + $('#answer').val() + $('#divide').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+    });
+	$('#product').click(function(e) { 
+	
+		if($('#answer').val() == ''){
+			return false;	
+			$('#equals').attr('onclick','');
+		}
+		else if ( $('#operation').attr('class') == 'activeAnswer') {
+			$('#operation').val( $('#operation').val() + $('#product').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+		else{
+			$('#operation').val( $('#operation').val() + $('#answer').val() + $('#product').val() );
+			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+    });	
+	$('#equals').click(function(){
+		
+		if($('#equals').attr('onclick') != 'return false'){
+		
+			var a = $('#answer').val();
+			var b = $('#operation').val();
+			var c = b.concat(a);
+			$('#answer').val(eval(c));
+			$('#operation').val(eval(c));
+			$('#operation').addClass('activeAnswer');
+			$('#equals').attr('onclick','return false');
+		
+		}
+    });
+});
